32543517bfe50127c88a54b0230c8c487059fb10
repl.it user <replituser@example.com>
2021-03-08 06:16:12 +0000

    Elif introduced

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Plese input the first number!")
+first_number=input()
+print("First number is", first_number)
+print("Plese input the second number!")
+second_number=input()
+print("Second number is", second_number)
+if(first_number>second_number):
+  print("First number is bigger!")
+elif(first_number<second_number):
+  print("Second number is bigger!")
+else:
+  print("Both are equal!")
+  
